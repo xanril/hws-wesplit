547369f162d997a39c30af02d30d75da4b7316d8
Kristian Mitra <xanril@users.noreply.github.com>
2022-05-18 12:26:16 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kristian Mitra on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
